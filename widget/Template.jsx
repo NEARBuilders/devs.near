51c37b9b3e82aa28fcbe323d83016fb46bc007ad
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-04-10 12:34:37 -0400

    update layout

---
+++ widget/Template.jsx
@@ -0,0 +1,26 @@
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Template = ({ layout, theme, blocks, children }) => {
+  const Layout = VM.require(layout?.src ?? "${config_account}/widget/Layout") || (() => <></>);
+
+  return (
+    <Container style={theme}>
+      <Layout
+        {...(layout?.initialProps ?? { variant: "standard" })}
+        blocks={blocks}
+      >
+        {children}
+      </Layout>
+    </Container>
+  );
+};
+
+return { Template };
