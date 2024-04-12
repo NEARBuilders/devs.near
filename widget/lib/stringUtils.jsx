7043132f9c86379715f1b5d02ec25fc3e8788152
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-04-11 21:23:47 -0400

    updates bos-workspace

--- widget/lib/stringUtils.jsx
+++ widget/lib/stringUtils.jsx
@@ -2,20 +2,26 @@
  * Transform input into a consistent and standardized format
  *
  * @param {string} text - The input to normalize.
+ * @param {string} delimiter - The delimiter to use between words.
  * @returns {string} - normalized input
  */
 
-const normalize = (text) =>
-  text
-    // Convert to lowercase
-    .toLowerCase()
-    // Replace spaces with dashes
-    .replace(/\s+/g, "-")
-    // Replace any non-alphanumeric characters (excluding dashes) with nothing
-    .replace(/[^a-z0-9-]/g, "")
-    // Replace multiple consecutive dashes with a single dash
-    .replace(/-+/g, "-")
-    // Trim dashes from the start and end of the string
-    .replace(/^-+|-+$/g, "");
+const normalize = (text, delimiter) => {
+  // If no delimiter is provided, default to an underscore
+  delimiter = delimiter || "_";
+  return (
+    text
+      // Convert to lowercase
+      .toLowerCase()
+      // Replace spaces with dashes
+      .replace(/\s+/g, delimiter)
+      // Replace any non-alphanumeric characters (excluding dashes) with nothing
+      .replace(`/[^a-z0-9${delimiter}]/g`, "")
+      // Replace multiple consecutive dashes with a single dash
+      .replace(`/${delimiter}+/g`, "-")
+      // Trim dashes from the start and end of the string
+      .replace(`/^${delimiter}+|${delimiter}+$/g`, "")
+  );
+};
 
 return { normalize };
